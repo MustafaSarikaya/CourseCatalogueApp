b838afe532e1a56c981edb50297746d731d8a88f
Zhong Tao <105749273+RE9757@users.noreply.github.com>
2022-06-06 16:27:25 -0400

    Add files via upload

---
+++ User.java
@@ -0,0 +1,56 @@
+package projcet;
+
+public abstract class User {
+
+    private String firstName, lastName, emailAddress, passWord;
+
+    public User(String firstName,String lastName, String emailAddress, String passWord){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.passWord = passWord;
+    }
+
+    public String toString(){
+        String result="";
+        result+=getFullName()+"\n"
+                +"Email Address: "+getEmailAddress()+"\n";
+        return result;
+    }
+
+    public String getFullName(){
+        return "Name: "+firstName+" "+lastName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName=lastName;
+    }
+
+    public String getEmailAddress(){
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress){
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassWord(){
+        return passWord;
+    }
+
+    public void setPassWord(String passWord){
+        this.passWord = passWord;
+    }
+}
