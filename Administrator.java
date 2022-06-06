73b23e05e434d034d8b0c098b862f4bf8b5966bb
Zhong Tao <105749273+RE9757@users.noreply.github.com>
2022-06-06 16:27:25 -0400

    Add files via upload

---
+++ Administrator.java
@@ -0,0 +1,77 @@
+package projcet;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class Administrator extends User {
+
+    private LinkedList<Course> courseList;
+    private LinkedList<Student> studentList;
+    private LinkedList<Instructor> InstructorList;
+
+    public Administrator(String firstName, String lastName, String emailAddress, String passWord) {
+        super(firstName, lastName, emailAddress, passWord);
+    }
+
+    public Instructor createInstructor(String firstName, String lastName, String emailAddress, String passWord) {
+        Instructor result = new Instructor(firstName, lastName, emailAddress, passWord);
+        return result;
+    }
+
+    public void addInstructor(Instructor instructor) {
+        InstructorList.add(instructor);
+    }
+
+    public void deleteInstructor(String instructorEmailAddress) {
+        for (ListIterator<Instructor> iter = InstructorList.listIterator(); iter.hasNext(); ) {
+            if (iter.next().getEmailAddress() == instructorEmailAddress) {
+                iter.remove();
+            }
+        }
+    }
+
+
+    public Student createStudent(String firstName, String lastName, String emailAddress, String passWord) {
+        Student result = new Student(firstName, lastName, emailAddress, passWord);
+        return result;
+    }
+
+    public void addStudent(Student student) {
+        studentList.add(student);
+    }
+
+    public void deleteStudent(String studentEmailAddress) {
+        for (ListIterator<Student> iter = studentList.listIterator(); iter.hasNext(); ) {
+            if (iter.next().getEmailAddress() == studentEmailAddress) {
+                iter.remove();
+            }
+        }
+    }
+
+    public Course creatCourse(String courseCode, String courseName) {
+        Course result = new Course(courseCode, courseName);
+        return result;
+    }
+
+    public void addCourse(Course course) {
+        courseList.add(course);
+    }
+
+    public void deleteCourse(String courseCode) {
+        for (ListIterator<Course> iter = courseList.listIterator(); iter.hasNext(); ) {
+            if (iter.next().getCourseCode() == courseCode) {
+                iter.remove();
+            }
+        }
+    }
+
+    public Course searchCourse(String courseCode){
+        for (ListIterator<Course> iter = courseList.listIterator(); iter.hasNext(); ) {
+            if (iter.next().getCourseCode() == courseCode) {
+                return iter.next();
+            }
+        }
+        throw new NoSuchElementException("cannot find this course!");
+    }
+}
