1d0379e19e1aec989f4329775eac18b162865218
Zhong Tao <105749273+RE9757@users.noreply.github.com>
2022-06-07 12:41:51 -0400

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,39 @@
+package projcet;
+
+public class Course {
+
+    private String courseCode, courseName, courseInfo;
+
+    public Course(String courseCode, String courseName){
+        this.courseCode=courseCode;
+        this.courseName=courseName;
+    }
+
+    public Course(String courseCode, String courseName, String courseInfo){
+        this.courseCode=courseCode;
+        this.courseName=courseName;
+        this.courseInfo=courseInfo;
+    }
+
+    public String getCourseCode(){
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode){
+        this.courseCode=courseCode;
+    }
+
+    public String getCourseName(){
+        return courseName;
+    }
+
+    public void setCourseName(String courseName){
+        this.courseName=courseName;
+    }
+
+    public String getCourseInfo(){
+        return courseInfo;
+    }
+
+    public void setCourseInfo(String courseInfo) { this.courseInfo=courseInfo;}
+}
