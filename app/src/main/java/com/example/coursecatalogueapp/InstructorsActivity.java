d894704b323735b51214260546c492aacc52a29e
talyavuruskaner <talya>
2022-06-14 14:44:39 +0300

    Instructor Main Page created

---
+++ app/src/main/java/com/example/coursecatalogueapp/InstructorsActivity.java
@@ -0,0 +1,21 @@
+package com.example.coursecatalogueapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class InstructorsActivity extends Activity {
+
+
+
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructors);
+
+
+    }
+
+}
